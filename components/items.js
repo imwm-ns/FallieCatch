c9be0a0c160774462559008fcdf2e5075be0bc18
imwm-ns <narathip_aot@hotmail.com>
2023-11-29 21:45:09 +0700

    Update a fist progress.

---
+++ components/items.js
@@ -0,0 +1,44 @@
+export const items = [
+    {
+        itemNo: 1,
+        src: require('../assets/level1/apple.png'),
+        speed: 6,
+        score: 2
+    },
+    {
+        itemNo: 2,
+        src: require('../assets/level1/bananas.png'),
+        speed: 6,
+        score: 2
+    },
+    {
+        itemNo: 3,
+        src: require('../assets/level1/dragon-fruit.png'),
+        speed: 8,
+        score: 3
+    },
+    {
+        itemNo: 4,
+        src: require('../assets/level1/grapes.png'),
+        speed: 4,
+        score: 1
+    },
+    {
+        itemNo: 5,
+        src: require('../assets/level1/passion-fruit.png'),
+        speed: 8,
+        score: 3
+    },
+    {
+        itemNo: 6,
+        src: require('../assets/level1/pineapple.png'),
+        speed: 10,
+        score: 4
+    },
+    {
+        itemNo: 7,
+        src: require('../assets/level1/strawberry.png'),
+        speed: 12,
+        score: 5
+    },
+]
